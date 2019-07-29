4a0f22846a564cbce3fc25a706a940fc126aa452
Mark Vrahas <vrahasm@gmail.com>
2019-07-28 17:35:23 -0700

    Code readability and adding description
    Added a link to a new tutorial that is helping guide the process. The app has ability to do a GET from localhost

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-// https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
+// Building an api with express js https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
+// Connect api to mongo db https://codeburst.io/building-a-rest-api-using-mongo-db-75cac3403fab
 
 var express = require("express");
 var app = express();
+//var bodyParser = require("body-parser");
+//var Activity = require("./models/activity");
+var mongoose = require("mongoose");
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://telemarkus:Vrahas128@telemarkus-rp9d8.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+
+
+
+
+// This is my function for connecting to mydb
 
 MongoClient.connect(url, { useNewUrlParser: true },
 	function(err, db){
@@ -12,27 +22,12 @@ MongoClient.connect(url, { useNewUrlParser: true },
 		var dbo = db.db("mydb");
 		var holes_collection = dbo.collection("holes");
 
-
-		//console.log("Check this out on hole 1:" + queryDocInCollection(holes_collection));
-		//insertDocIntoCollection({hole_number: "5", par: "5", score: "4"}, holes_collection)
-		queryDocInCollection({}, dbo, holes_collection);
-		/*
-		holes_collection.findOne({}, 
-			function(err, result){
-				if (err) throw err;
-				console.log(result.score);
-			}
-		);
-		*/
-
+		//insertDocIntoCollection({hole_number: "5", par: "5", score: "4", other_category: "7"}, holes_collection)
+		//queryDocInCollection({}, dbo, holes_collection);
 
 		db.close();
 	}    
 );
-
-
-
-
 
 
 //This is my function for inserting data
@@ -46,6 +41,7 @@ function insertDocIntoCollection(doc, collection) {
 	);
 }
 
+
 //This is my function for querying data
 
 function queryDocInCollection(query, database, collection){
@@ -56,31 +52,27 @@ function queryDocInCollection(query, database, collection){
 		}
 	);
 }
-//https://stackoverflow.com/questions/45118957/node-js-mongodb-collection-find-toarray-returns-nothing
-//Possible solution for returning the queried data to use in a controller
+//Possible solution for returning the queried data to use in a controller https://stackoverflow.com/questions/45118957/node-js-mongodb-collection-find-toarray-returns-nothing
+
+
+
+
+
 
 
 /*This is the basic code required to return a json response*/
 
-app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use(bodyParser.json());
+
+app.get("/url", 
+	function(req, res){
+		res.json({message: "This is what we are returning."});
+	}
+);
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
 
-
-
-
-
-
-/*
-
-Steps to create a Node project
-
-
-
-
-*/
