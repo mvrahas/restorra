d741224ebd2a6256a4847ce8a09b6d9206701dfa
Mark Vrahas <vrahasm@gmail.com>
2019-08-03 11:59:53 -0700

    Dev/ Staging / Prod support
    Added Config.js to specify variables for Dev / Staging / Prod environments

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 // Building an api with express js https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
 // Connect api to mongo db https://codeburst.io/building-a-rest-api-using-mongo-db-75cac3403fab
 // Use mongoode to create data models and api methods https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
+// Config for managing variables in different environments https://codeburst.io/node-js-best-practices-smarter-ways-to-manage-config-files-and-variables-893eef56cbef
+
+process.env.NODE_ENV = 'staging';
+
+const _ = require('lodash');
+const config = require('./config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
+const finalConfig = _.merge(defaultConfig, environmentConfig);
+global.gConfig = finalConfig;
+console.log(finalConfig.node_port);
+
+
+
 
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000;
+var port = finalConfig.node_port;
 var mongoose = require('mongoose');
 var Hole = require('./api/models/appModel');
 var bodyParser = require("body-parser");
 
 //mongodb driver
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://telemarkus:Vrahas128@telemarkus-rp9d8.gcp.mongodb.net/golf_app?retryWrites=true&w=majority";
+var url = finalConfig.database_url;
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
