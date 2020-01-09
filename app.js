f8f18975d10243d7347a6b82d1ae0ce98b3264f3
Mark Vrahas <vrahasm@gmail.com>
2020-01-08 19:28:31 -0800

    Hello World

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const add = 2+3
-
-console.log(add)
+const express = require('express')
+const chalk = require('chalk')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000, () => {console.log(chalk.green('App is up on 3000'))})
