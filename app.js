82f012e4d9c2c5e60788718dc5a4a783e33cf2de
Mark Vrahas <vrahasm@gmail.com>
2020-01-08 21:05:17 -0800

    =Connected to and add record to database

--- app.js
+++ app.js
@@ -1,9 +1,68 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const chalk = require('chalk')
 const app = express()
+
+const port = process.env.PORT || 3000
  
-app.get('/', function (req, res) {
+
+
+
+
+
+//mongoose stuff
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, dbName: 'golf-app', useUnifiedTopology: true }).
+  catch((error) => {console.log(error)})
+
+var db = mongoose.connection;
+db.on('error', err => {
+  console.log('error')
+});
+db.once('open', function() {
+  console.log('connected')
+});
+
+var scoreSchema = new mongoose.Schema({
+  name: String
+});
+
+var Score = mongoose.model('Score', scoreSchema);
+
+
+
+
+
+
+
+
+
+
+
+//express stuff
+
+app.listen(port, () => {console.log(chalk.green('App is up on ' +port))})
+
+
+
+app.get('/', (req, res) => {
   res.send('Hello World')
 })
- 
-app.listen(3000, () => {console.log(chalk.green('App is up on 3000'))})
+
+app.get('/scores', (req, res) => {
+  res.send('This is where the scores will be listed')
+})
+
+app.post('/scores/:score', (req, res) => {
+
+  var entry = new Score({ name: req.params.score });
+
+  entry.save((err, entry) => {
+    if (err) {
+      console.log(err)
+      res.status(400).send()
+    }
+    res.status(200).send(entry)
+  })
+
+})
