b465eb398c429355caac70fd64a5cf5a1bd437ba
Mark Vrahas <vrahasm@gmail.com>
2020-01-15 19:52:27 -0800

    Added 'get' method for scores

--- src/routes/score.js
+++ src/routes/score.js
@@ -2,21 +2,45 @@ const Score = require('../models/score.js')
 const express = require('express')
 const router = express.Router()
 
-router.get('/scores', (req, res) => {
-    res.send('This is where the scores will be listed')
-  })
+router.get('/scores', async (req, res) => {
   
-router.post('/scores', (req, res) => {
+  // Implementation with callbacks
+
+  // Score.find({}, function (err, result) {
+  //   if (err) {
+  //     return res.status(400).send()
+  //   }
+  //   return res.send(result)
+  // })
 
 
-var entry = new Score(req.body);
-    entry.save((err, entry) => {
-      if (err) {
-        console.log(err)
-        res.status(400).send()
-      }
-      res.status(200).send(entry)
-    })
+  // Implementation with promises
+  
+  // Score.find({}).then((result) => {
+  //   res.status(200).send(result)
+  // }).catch((err) => {
+  //   res.status(400).send('There was an issue')
+  // })
+  
+  // Implementation with async await
+  try {
+    const scores = await Score.find({})
+    res.status(200).send(scores)
+  } catch (e) {
+    res.status(500).send(e)
+  }
+})
+  
+router.post('/scores', async (req, res) => {
+
+  var entry = new Score(req.body)
+  try {
+    await entry.save()
+    res.status(200).send(entry)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+
 })
 
 module.exports = router
