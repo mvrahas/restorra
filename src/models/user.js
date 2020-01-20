a1af1f47a26f3ec8e8d6a9f6e66d20db3f825758
Mark Vrahas <vrahasm@gmail.com>
2020-01-19 17:48:33 -0800

    Convert passwords to hash before saving

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true);
 const validator = require('validator')
+const bcrypt = require('bcrypt');
 
 userSchema = new mongoose.Schema({
 
@@ -30,6 +31,15 @@ userSchema = new mongoose.Schema({
         required: false
     },
 })
+
+userSchema.pre('save', async function(next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next()
+});
+
 
 const User = mongoose.model('User', userSchema);
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,14 +2,19 @@ const express = require('express')
 const User = require('../models/user')
 const router = express.Router()
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+
+
+
+router.post('/register', async (req, res) => {
+  
+  const user = new User(req.body)
     try {
       await user.save()
       res.status(201).send(user)
     } catch (e) {
       res.status(400).send(e)
     }
+
   })
 
 router.get('/users', async (req, res) => {
