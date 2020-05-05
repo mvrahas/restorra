5fc84933bbb2da4385d9088824bdf83365c18a32
Mark Vrahas <vrahasm@gmail.com>
2020-05-05 16:24:18 -0700

    Cleanup and optimization

---
+++ web/public/js/network-request.js
@@ -0,0 +1,59 @@
+function getCookie(name) {
+    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+}
+
+
+const token = getCookie('token')
+
+get = async function(route) {
+
+    const options = {
+        headers: {
+            "Authorization" : "Bearer " + token,
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+        }
+    }
+
+    try {
+        const data = await fetch(route,options)
+        const json_data = data.json()
+        if(json_data.error) {
+            return json_data.error
+        } else {
+            console.log(json_data)
+            return json_data
+        }
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+
+post = async function(body,route) {
+
+    const options = { 
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    }
+    
+    try {
+        const data = await fetch(route,options)
+        const json_data = data.json()
+        if(json_data.error) {
+            return json_data.error
+        } else {
+            console.log(json_data)
+            return json_data
+        }
+    } catch (e) {
+        console.log(e)
+    }
+
+}
