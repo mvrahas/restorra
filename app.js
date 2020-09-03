27de86c2911ca81d5b27958c3d5468d8c1407c48
telemarkus <vrahasm@gmail.com>
2020-09-02 20:41:04 -0700

    oops

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
-const environment = process.env.DEPLOYMENT_ENV || 'local'
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
