270f9d843c6881a99d24eaf5c97929441aaa059b
telemarkus <vrahasm@gmail.com>
2020-09-05 12:39:54 -0700

    added selection page to customize menu

--- app.js
+++ app.js
@@ -12,35 +12,84 @@ app.set('views', viewsPath)
 
 
 
-app.get('/', (req, res) => {
+app.get('/menu', async (req, res) => {
 
-  axios.get('https://v2-api.sheety.co/c8d0b3b214a817554114d96220e3c881/restoAdds/config')
-  .then(function (response) {
-    // handle success
-    console.log(response.data.config[0].id)
+  const restaurantIndex = parseInt(req.query.restaurant) - 2 || 0
+  const addIndex = parseInt(req.query.add) - 2 || 0
+
+  try {
+    const response1 = await axios.get('https://v2-api.sheety.co/c8d0b3b214a817554114d96220e3c881/restoAdds/restaurants')
+    const restaurant = response1.data.restaurants[restaurantIndex]
+    const response2 = await axios.get('https://v2-api.sheety.co/c8d0b3b214a817554114d96220e3c881/restoAdds/adds')
+    const add = response2.data.adds[addIndex]
 
     res.render('index', {
-      background: response.data.config[0].restaurantBackgroundUrl,
-      name: response.data.config[0].restaurantTitle,
-      subtitle: response.data.config[0].restaurantSubtitle,
-      restaurantlogo: response.data.config[0].restaurantLogoUrl,
-      addcaption: response.data.config[0].addCaption,
-      addimage: response.data.config[0].addImageUrl,
-      addlink: response.data.config[0].addLink
-  })
+      background: restaurant.restaurantBackgroundUrl,
+      name: restaurant.title,
+      subtitle: restaurant.restaurantSubtitle,
+      restaurantlogo: restaurant.restaurantLogoUrl,
+      addcaption: add.title,
+      addimage: add.addImageUrl,
+      addlink: add.addLink
+    })
+  } catch {
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-    res.send('opps')
-  }).then(function () {
-    
-  });
-
-
+  }
 
 })
+
+
+app.get('/selectrestaurant', async (req, res) => {
+  
+  const nextLink = "/selectadd?"
+  const type = "restaurant"
+
+  try {
+
+    const response = await axios.get('https://api.sheety.co/c8d0b3b214a817554114d96220e3c881/restoAdds/restaurants')
+    const restaurants = response.data.restaurants
+    for (var i=0; i < restaurants.length; i++) {
+      restaurants[i].nextLink = nextLink
+      restaurants[i].type = type
+    }
+    res.render('selection', {
+      items: restaurants,
+      type: type
+    })
+
+  } catch (e) {
+    res.send(e)
+  }
+
+})
+
+app.get('/selectadd', async (req, res) => {
+  const nextLink = "/menu?restaurant=" + req.query.restaurant + "&"
+  const type = "add"
+
+  try {
+
+    const response = await axios.get('https://api.sheety.co/c8d0b3b214a817554114d96220e3c881/restoAdds/adds')
+    const adds = response.data.adds
+    for (var i=0; i < adds.length; i++) {
+      adds[i].nextLink = nextLink
+      adds[i].type = type
+    }
+    res.render('selection', {
+      items: adds,
+      type: type
+    })
+
+  } catch (e) {
+    res.send(e)
+  }
+
+
+
+  
+})
+
+
 
 app.use(express.static('public'))
 
